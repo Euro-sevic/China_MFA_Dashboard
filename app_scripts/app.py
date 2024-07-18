1a9bb79380969a4198a41ed88b88cdf95f83b7ba
Euro-sevic <101292969+Euro-sevic@users.noreply.github.com>
2024-07-18 22:41:38 +0200

    Add dataset selection functionality to Streamlit app
    - Added radio button in the sidebar for users to choose between the full dataset and the Ukraine-only dataset.
    - Updated data loading logic to load the selected dataset based on user input.

--- app_scripts/app.py
+++ app_scripts/app.py
@@ -9,16 +9,19 @@ import pickle
 st.set_page_config(layout="wide")
 
 @st.cache_data
-def load_data():
+def load_data(dataset='full'):
     base_path = os.path.dirname(__file__)
-    file_path = os.path.join(base_path, '..', 'data', 'CMFA_PressCon_v4.xlsx')
+    if dataset == 'full':
+        file_path = os.path.join(base_path, '..', 'data', 'CMFA_PressCon_v4.xlsx')
+    else:
+        file_path = os.path.join(base_path, '..', 'data', 'ukraine_dataset_combined.xlsx')
+        
     df = pd.read_excel(file_path)
     columns_to_clean = ["a_per", "a_loc", "a_org", "a_misc"]
     for column in columns_to_clean:
         df[column] = df[column].replace("-", np.nan).astype(str)
     return df
 
-data = load_data()
 
 @st.cache_data
 def load_precomputed_stats():
@@ -105,6 +108,13 @@ unique_miscellaneous = sorted(set(item for sublist in data['a_misc'].dropna().ap
 
 with st.sidebar:
     st.title("What does the official China say about...?")
+    
+    dataset_choice = st.radio(
+        "Choose Dataset:",
+        ('Full Dataset', 'Ukraine-only Dataset'),
+        index=0
+    )
+    
     st.markdown(
         """
     This interactive dashboard allows you to explore a corpus of the Chinese Ministry of Foreign Affairs press conferences. The dataset is a unique source of information for 20+ years of China's foreign policy discourse. Select different criteria to get insights from the data.
@@ -122,6 +132,9 @@ with st.sidebar:
         help="Choose 'AND' to display entries that meet all criteria or 'OR' for entries that meet any of the selected criteria."
     )
 
+# Adjust this part to load the dataset based on user selection
+dataset = 'full' if dataset_choice == 'Full Dataset' else 'ukraine'
+data = load_data(dataset)
 
 def filter_data(people, organizations, locations, miscellaneous, logic_type):
     conditions = []
